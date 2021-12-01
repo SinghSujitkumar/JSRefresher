01a36709928bc9354a986db73f2b80b37ecfbf47
CSIVESIT <tinachandwani94@gmail.com>
2021-12-01 21:11:15 +0530

    day2-01

--- sandbox.js
+++ sandbox.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+
+'use strict';
+
+
+
+let hasDriverLicense = false;
+const passtest = true;
+if(passtest) hasDriverLicense = true;
+if (hasDriverLicense) console.log("I can drive");
+
+// const interface = 'Audio';
+const private = 3;
